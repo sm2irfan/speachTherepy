aa314a9272a9f7fb9332a8f011dd11396f9e9eb3
Mohamed Akeel <akeel1447@gmail.com>
2022-04-20 05:43:54 +0530

    Creating circle icon button component

---
+++ src/components/CircleButton.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { TouchableOpacity, StyleSheet, Image } from 'react-native';
+
+const CircleButton = ({ icon, onPress }) => {
+	return (
+		<TouchableOpacity
+			style={styles.circleButton}
+			onPress={onPress}
+			activeOpacity={0.7}>
+			<Image style={styles.icon} source={icon} />
+		</TouchableOpacity>
+	);
+};
+
+const styles = StyleSheet.create({
+	circleButton: {
+		width: 55,
+		height: 55,
+		padding: 10,
+		marginTop: 5,
+		borderRadius: 100 / 2,
+		marginHorizontal: 5,
+		backgroundColor: 'rgba(52, 52, 52, 0.5)',
+	},
+	icon: {
+		height: '100%',
+		width: 'auto',
+	},
+});
+
+export { CircleButton };
